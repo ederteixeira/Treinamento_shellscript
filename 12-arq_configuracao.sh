e2a9713f56637f9040b2089c45964cf9f348edf3
ederteixeira <ccbeder@gmail.com>
2021-11-06 17:42:25 -0300

    Adicionando função

--- 12-arq_configuracao.sh
+++ 12-arq_configuracao.sh
@@ -10,7 +10,8 @@
 # Histórico:
 #     v1.0 06/11/2021 - Éder
 #       -Inicio do programa
-#       -Alteração:
+#     v1.1 06/11/2021 - Éder
+#       -Alteração: Inclusão da função.
 # --------------------------------------------------------------------- #
 #   Testado em:
 #   bash 5.0.17(1)
@@ -19,21 +20,30 @@ ARQ_CONFIG="12-conf.cf"
 MAIUSCULO=
 COR=
 MSG="Mensagem de teste"
-
 VERMELHO="\033[31;1;5m"
 AZUL="\033[34;1;5m"
 # ---------------------------- Testes --------------------------------- #
 [ ! -r "$ARQ_CONFIG" ] && echo "sem acesso a leitura" && exit 1
 
 # --------------------------- Funções --------------------------------- #
-#
-#
+DefParametro(){
+  local param="$(echo $1 | cut -d = -f 1)"
+  local valor="$(echo $1 | cut -d = -f 2)"
+
+  case "$param" in
+    COR)       COR="$valor"      ;;
+    MAIUSCULO) MAIUSCULO="$valor";;
+  esac
+}
+
 # -------------------------- Execução --------------------------------- #
 while read -r linha
 do
   [ "$(echo $linha | cut -c1)" = "#" ] && continue
   [ ! "$linha" ] && continue
-  echo "$linha"
+  DefParametro "$linha"
 done < "$ARQ_CONFIG"
-#
+
+echo "O valor da cor é: $COR"
+echo "O valor do maiusculo é: $MAIUSCULO"
 #
